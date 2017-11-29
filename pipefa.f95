770c8fd7521ea88f10ca6f0ed3c8d9e1896fd8e7
tfjones24 <tfjones@mines.edu>
2017-11-29 08:32:20 -0700

    Finished up 1,2 subroutines

--- pipefa.f95
+++ pipefa.f95
@@ -26,7 +26,7 @@ integer::function = 0, flowRegime = 0, counter=0
 real::pipDiam, pipLen, pipVel, pipRough, reynolds, flow, friction, head, kinVisco=0.0000008297, pi=3.14159265359, g=9.81
 character(len=20)::pipeType
 character(len=50),dimension(2)::temp
-logical::temp=.false.
+logical::templog=.false.
 
 type roughnessTable
         character(len=20),dimension(8) :: types
@@ -43,7 +43,7 @@ open(unit=100, file='table.txt', status='OLD', action='READ')
         do counter=0,7
                read(100,*) temp
                table%types(counter) = temp(1)
-               table%values(counter) = temp(2)
+               read(trim(temp(2)), *) table%values(counter)
         end do
 
                
@@ -59,15 +59,15 @@ do while (1==1)
         write(*,*) ":-------------------------------------------------------"
         read(*,*) function
         select case (function)
-                case 1
+                case (1)
                         CALL headloss(table)
-                case 2
+                case (2)
 !			Call flowrate
-                case 3
+                case (3)
 !			call pipediam	
                 case default
                    write(*,*) "That is not a valid option"
- 
+	end select 
 
 end do
 ! Say Goodbye
@@ -83,9 +83,9 @@ implicit none
 
 integer::function = 0, flowRegime = 0, counter=0
 real::pipDiam, pipLen, pipVel, pipRough, reynolds, flow, friction, head, kinVisco=0.0000008297, pi=3.14159265359, g=9.81
-character(len=20)::pipeType
+character(len=20)::pipType
 character(len=50),dimension(2)::temp
-logical::temp=.false.
+logical::templog=.false.
 
 type(roughnessTable) :: table
 
@@ -93,14 +93,15 @@ type(roughnessTable) :: table
 write(*,*) "Please enter pipe diameter, velocity, and length in that order:"
 read(*,*) pipDiam, pipVel, pipLen
 write(*,*) "Great, now we need the pipe type:"
-do while temp==.FALSE.
+do while (templog.EQV..FALSE.)
+
      do counter=1,8
-          if trim(table%types(counter))==trim(pipType)
-               temp=.true.
+          if (trim(table%types(counter))==trim(pipType)) then
+               templog=.true.
                pipRough=table%values(counter)
           end if
      end do
-     if temp==.false.
+     if (templog.EQV..false.) then
           write(*,*) "Please enter a valid type."
           write(*,*) table%types
      end if
@@ -113,9 +114,9 @@ WRITE(*,*) "Our relative roughness is ", pipRough
 
 flow=pipVel*(pi*0.25*pipDiam*pipDiam)
 
-IF (reynolds >= 4000)
+IF (reynolds >= 4000) then
      friction=0.25/(LOG((1/(3.7*(1/pipRough))) + (5.74/(reynolds**0.9)))**2)
-ELSE IF (reynolds <= 2000) 
+ELSE IF (reynolds <= 2000) then 
      friction=64/reynolds
 ELSE
      WRITE(*,*) "Transitional Flow is not supported by this program"
@@ -130,8 +131,93 @@ end subroutine headloss
 
 
 
-subroutine flowrate()
-implicit none
+
+subroutine flowrate(table, flow)
+
+real::diameter, headloss
+character(len=20)::pipetype
+type(roughnessTable), intent(in)::table
+real, intent(out)::flow
+integer::err, i=0 
+real::roughness,v
+real::relrough, NR
+real::f,A, length,temp  !friction factor
+real, parameter::viscosity  = 0.0000008297
+
+
+WRITE(*,*) 'please enter the type of pipe'
+READ(*,*) pipetype
+
+DO i=1,8
+
+        IF(table%types(i) == pipetype)THEN
+              roughness = table%values(i)
+	ELSE
+		WRITE(*,*)'plastic'
+		WRITE(*,*)'drawn'
+		WRITE(*,*)'steel'
+		WRITE(*,*)'galvanized_iron'
+		WRITE(*,*)'coated_iron'
+		WRITE(*,*)'uncoated_iron'
+		WRITE(*,*)'concrete'
+		WRITE(*,*)'riveted_steel'
+		WRITE(*,*)'Enter pipe type from the above table.'
+		READ(*,*) pipetype
+	END IF
+	
+END DO
+	WRITE(*,*)
+	WRITE(*,*) 'What is the desired headloss?'
+	READ(*,*) headloss
+	WRITE(*,*)
+	WRITE(*,*) 'what is the diameter of the pipe?'
+	READ(*,*) diameter
+	WRITE(*,*)
+	WRITE(*,*)'what is the length of the pipe?'
+	READ(*,*) length
+	
+	
+!actual procedure starts here
+! Calculating relative roughness
+
+relrough = diameter/roughness
+
+f = 0.01
+
+A = sqrt((2*g*diameter*headloss)/length)
+
+DO 
+	f = temp
+	
+	v = A/(sqrt(f))
+	
+	NR = (v*diameter)/viscosity  	! calculated friction factor  
+! do if statement to see if flow is laminar, turbulent or transitional if transition tell the user to go !away
+	IF (NR <= 2000) THEN  !flow is laminar
+		f = 64./NR
+	ELSE IF(NR > 2000 .AND. NR < 4000)THEN  ! flow is trasnssitional
+		WRITE(*,*) 'flow is transitional, cannot calculate flow'
+		STOP
+	ELSE
+		f=0.25/(LOG((1/(3.7*(1/relrough))) + (5.74/(NR**0.9)))**2)
+	END IF
+	
+	IF( abs(temp-f)/f <= 0.01) EXIT
+	
+	
+END DO
+! now we have the value for the f
+! now solve for the velocity
+
+v = A/(sqrt(f))
+
+! now we solve for the flow 
+
+flow  = v*3.14159*(diameter**2)
+
+
+
+
 end subroutine flowrate
 
 
@@ -139,8 +225,34 @@ end subroutine flowrate
 
 
 
-subroutine pipediam()
+subroutine pipediam(table)
 implicit none
+integer::function = 0, flowRegime = 0, counter=0
+real::pipDiam, pipLen, pipVel, pipRough, reynolds, flow, friction, head, kinVisco=0.0000008297, pi=3.14159265359, g=9.81
+character(len=20)::pipType
+character(len=50),dimension(2)::temp
+logical::templog=.false.
+
+type(roughnessTable) :: table
+
+
+
+write(*,*) "First we'll get the pipe roughness."
+do while (templog.EQV..FALSE.)
+     do counter=1,8
+          if (trim(table%types(counter))==trim(pipType)) then
+               templog=.true.
+               pipRough=table%values(counter)
+          end if
+     end do
+     if (templog.EQV..false.) then
+          write(*,*) "Please enter a valid type."
+          write(*,*) table%types
+     end if
+end do
+
+
+
 end subroutine pipediam
 
 
